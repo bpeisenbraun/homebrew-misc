9eabc88ff2734e162409555b3190740109fedb0b
Ben Eisenbraun <bene@klatsch.org>
2014-09-09 17:36:32 -0400

    Initial commit of ssh2 formula.

---
+++ README.md
@@ -0,0 +1,12 @@
+# SSH2 Homebrew Package
+
+This is built from the last open source version of the SSH.COM sources.
+
+Only the ssh2, scp2, sftp2, ssh-keygen2, ssh-agent2 and ssh-add2 binaries will
+be installed.
+
+To use it:
+
+```
+brew install http://
+```
---
+++ ssh2.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Ssh2 < Formula
+  homepage "http://fossies.org/linux/misc/old/ssh-3.2.9.1.tar.gz/"
+  url "http://fossies.org/linux/misc/old/ssh-3.2.9.1.tar.gz"
+  sha1 "22e4589c889aef18e53a8942716084f0d8ede7db"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-foreign-etcdir=#{prefix}/etc",
+                          "--prefix=#{prefix}"
+    system "make"
+    bin.install "apps/ssh/ssh2", "apps/ssh/scp2", "apps/ssh/sftp2", 
+                "apps/ssh/ssh-agent2", "apps/ssh/ssh-keygen2", 
+                "apps/ssh/ssh-add2"
+  end
+end
